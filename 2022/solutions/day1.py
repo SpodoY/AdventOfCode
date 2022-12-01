7e35de4173db6f3620e953776f45dbe11baf9f29
Thomas Meier <thommy2002@outlook.com>
2022-12-01 06:17:11 +0100

    Wanky solution that may or may not contain a bug but works

--- 2022/solutions/day1.py
+++ 2022/solutions/day1.py
@@ -1,4 +1,24 @@
-with open("../inputs/in1.txt") as puzzle_input:
+import numpy as np
+
+with open("../inputs/testInput.txt") as puzzle_input:
     puzzle_input = [line.rstrip('\n') for line in puzzle_input.readlines()]
 
-print(puzzle_input)
+max_snacks_elfes = []
+curMax = 0
+for capacity in puzzle_input:
+    if capacity == '':
+        max_snacks_elfes.append(curMax)
+        curMax = 0
+        continue
+    curMax += int(capacity)
+    print(capacity)
+
+
+max_snacks_elfes = np.sort(max_snacks_elfes)
+
+print(max_snacks_elfes)
+# print(max_snacks_elfes[-1])
+# print(max_snacks_elfes[-2])
+# print(max_snacks_elfes[-3])
+
+print(max_snacks_elfes[-1] + max_snacks_elfes[-2] + max_snacks_elfes[-3])
