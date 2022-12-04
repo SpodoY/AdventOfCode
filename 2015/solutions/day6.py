e8f75b977ef0795eb59763f6c6861a5d245ac3e8
Thomas Meier <thommy2002@outlook.com>
2022-12-04 23:20:07 +0100

    Day 6 fully done

--- 2015/solutions/day6.py
+++ 2015/solutions/day6.py
@@ -1,10 +1,41 @@
-with open("../inputs/testInput.txt") as puzzle_input:
+with open("../inputs/in6.txt") as puzzle_input:
     puzzle_input = [x.rstrip() for x in puzzle_input.readlines()]
 
-print(puzzle_input)
+# print(puzzle_input)
+
+def analyze_instruction(instruction):
+    s = {}
+    split = [y.split(',') for y in [x for x in instruction.split(' ')] if len(y.split(',')) == 2]
+    s['xc'] = [int(split[0][0]), int(split[1][0])]
+    s['yc'] = [int(split[0][1]), int(split[1][1])]
+    if 'on' in instruction: s['action'] = 'on'
+    if 'off' in instruction: s['action'] = 'off'
+    if 'toggle' in instruction: s['action'] = 'toggle'
+    return s
+
 
 def part_one():
-    return
+    lights = [[-1 for _ in range(1000)] for _ in range(1000)]
+    for instruction in puzzle_input:
+        infos = analyze_instruction(instruction)
+        for col in range(infos['yc'][0], infos['yc'][1] + 1):
+            for row in range(infos['xc'][0], infos['xc'][1] + 1):
+                if infos['action'] == 'toggle': lights[col][row] *= -1
+                if infos['action'] == 'on': lights[col][row] = 1
+                if infos['action'] == 'off': lights[col][row] = -1
+    print(sum([x.count(1) for x in lights]))
 
 
-part_one()
+def part_two():
+    lights = [[0 for _ in range(1000)] for _ in range(1000)]
+    for instruction in puzzle_input:
+        infos = analyze_instruction(instruction)
+        for col in range(infos['yc'][0], infos['yc'][1] + 1):
+            for row in range(infos['xc'][0], infos['xc'][1] + 1):
+                if infos['action'] == 'toggle': lights[col][row] += 2
+                if infos['action'] == 'on': lights[col][row] += 1
+                if infos['action'] == 'off' and lights[col][row] > 0: lights[col][row] -= 1
+    print(sum(list(map(sum, lights))))
+
+# part_one()
+part_two()
