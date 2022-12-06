116e89f0f20c9e757dcd8edb2616e411b3dbd9be
Thomas Meier <thommy2002@outlook.com>
2022-12-06 17:14:55 +0100

    Day 6 fully done

---
+++ 2022/solutions/day6.py
@@ -0,0 +1,12 @@
+with open("../inputs/in6.txt") as puzzle_input:
+    puzzle_input = puzzle_input.readline()
+
+
+def solver(msg_length):
+    for seq in range(0, len(puzzle_input)):
+        if len({*puzzle_input[seq:seq + msg_length]}) == msg_length:
+            return seq + msg_length
+
+
+print(f'Part 1:  {solver(4)}')
+print(f'Part 1:  {solver(14)}')
